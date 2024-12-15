0603b93f36ff4ff3c46d58f2d27385d0808ce3c9
khakimov <khakimovmy@gmail.com>
2024-12-16 01:51:15 +0500

    Lesson1 Task1

--- src/main/kotlin/lesson_1/lesson1_task1.kt
+++ src/main/kotlin/lesson_1/lesson1_task1.kt
@@ -5,7 +5,6 @@ fun main() {
     val numberOfOrders: Int = 75
     val thankYouText: String = "Спасибо за вашу покупку!"
 
-
     println("Количество заказов составило $numberOfOrders штук!")
     println(thankYouText)
 
