c2aa95e96691fceef2af35f63649d29916fa8fe7
khakimov <khakimovmy@gmail.com>
2024-12-16 02:23:13 +0500

    KS-1-1 TASK1

---
+++ src/main/kotlin/lesson_1/lesson1_task2.kt
@@ -0,0 +1,17 @@
+package lesson_1
+
+fun main() {
+
+    val numberOfOrders: Int = 75
+    val thankYouText: String = "Спасибо за вашу покупку!!!"
+    var numberOfWorkers: Int = 2000
+
+    println("Количество заказов составило $numberOfOrders штук!")
+    println(thankYouText)
+
+    println("Количество сотрудников состаляет: $numberOfWorkers человек") //2000
+
+    numberOfWorkers = 1999
+    println("Один сотрудник уволился, осталось $numberOfWorkers человек")
+
+}
