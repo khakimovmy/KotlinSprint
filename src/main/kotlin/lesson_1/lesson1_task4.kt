d686f9364747b229b5ae4cb4d21eabe17997686e
khakimov <khakimovmy@gmail.com>
2024-12-16 03:13:17 +0500

    KS-1-4 TASK4

---
+++ src/main/kotlin/lesson_1/lesson1_task4.kt
@@ -0,0 +1,19 @@
+package lesson_1
+
+fun main() {
+
+    val length: Long = 40_868_600_000
+    val age: Byte = 27
+    val partOfDay: Double = 0.075
+    val seconds: Int = 6480
+    val partOfYear: Double = 2.0547945205479453E-4
+    val apogee: Int = 327_000
+
+    println("Расстояние: $length")
+    println("Возраст: $age")
+    println("Часть дня: $partOfDay")
+    println("Секунды: $seconds")
+    println("Часть года: $partOfYear")
+    println("Апогей орбиты: $apogee")
+
+}
