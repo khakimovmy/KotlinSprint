cf864618981bda3298fd86d3f523fff70dbfe6fb
khakimov <khakimovmy@gmail.com>
2024-12-16 02:45:51 +0500

    KS-1-3 TASK3

---
+++ src/main/kotlin/lesson_1/lesson1_task3.kt
@@ -0,0 +1,22 @@
+package lesson_1
+
+import kotlin.math.min
+
+fun main() {
+
+    val year = 1961
+    var hour: String = "09"
+    var minute: String = "07"
+
+    println(year)
+    println(hour)
+    println(minute)
+
+//    println("Время взлета - $year год в $hour:$minute")
+
+    hour = "10"
+    minute = "55"
+
+    println("Время посадки - в $hour:$minute")
+
+}
