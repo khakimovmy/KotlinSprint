fee6ff686ac4cffc4197d440739568074a64a6fb
khakimov <khakimovmy@gmail.com>
2024-12-17 00:23:22 +0500

    KS-1-3 Landing time

--- src/main/kotlin/lesson_1/lesson1_task3.kt
+++ src/main/kotlin/lesson_1/lesson1_task3.kt
@@ -3,19 +3,16 @@ package lesson_1
 fun main() {
 
     val year: Int = 1961
-    var hour: Int = 9
-    var minute: Int = 7
+    var hour: String = "09"
+    var minute: String = "07"
 
-    println(year)
+    println("$year год")
     println(hour)
     println(minute)
+    println("Время взлета - $year год в $hour:$minute")
 
-//    println("Время взлета - $year год в $hour:$minute")
+    hour = "10"
+    minute = "55"
+    println("Время посадки - в $hour:$minute")
 
-    hour = 10
-    minute = 55
-
-//    println("Время посадки - в $hour:$minute")
-
-    println("$hour:$minute")
 }
