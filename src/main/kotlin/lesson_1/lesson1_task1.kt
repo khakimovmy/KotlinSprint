6daf20c76060a056339b8213fe4f6ece62d93b12
khakimov <khakimovmy@gmail.com>
2024-12-16 23:42:06 +0500

    KS-1-1 Number of Orders

--- src/main/kotlin/lesson_1/lesson1_task1.kt
+++ src/main/kotlin/lesson_1/lesson1_task1.kt
@@ -3,9 +3,9 @@ package lesson_1
 fun main() {
 
     val numberOfOrders: Int = 75
-    val thankYouText: String = "Спасибо за вашу покупку!!!"
+    val thankYouMessage: String = "Спасибо за вашу покупку!!!"
 
     println("Количество заказов составило $numberOfOrders штук!")
-    println(thankYouText)
+    println(thankYouMessage)
 
 }
