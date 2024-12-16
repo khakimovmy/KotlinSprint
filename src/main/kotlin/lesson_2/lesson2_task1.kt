3f7ece0be9ad47ef8d99aaf63c16f2f6dbf8dcb9
khakimov <khakimovmy@gmail.com>
2024-12-17 01:10:36 +0500

    KS-2-1 Average English Score

---
+++ src/main/kotlin/lesson_2/lesson2_task1.kt
@@ -0,0 +1,14 @@
+package lesson_2
+
+fun main() {
+
+    val student1: Int = 3
+    val student2: Int = 4
+    val student3: Int = 3
+    val student4: Int = 5
+
+    val averageScore: Double = (student1 + student2 + student3 + student4) / 4.0
+
+    println("Средний балл по английскому этого класса - $averageScore")
+
+}
