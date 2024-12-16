719cdcc0f832722309347cf56cc19ecd2a218a5d
khakimov <khakimovmy@gmail.com>
2024-12-17 00:48:27 +0500

    KS-1-5 Time in cosmos

--- src/main/kotlin/lesson_1/lesson1_task5.kt
+++ src/main/kotlin/lesson_1/lesson1_task5.kt
@@ -1,17 +1,19 @@
 package lesson_1
 
 fun main() {
-    // Количество секунд, проведенных Гагариным в космосе
+
     val totalSeconds: Int = 6480
 
-    // Расчет времени
-    val hours: Int = totalSeconds / 3600
-    val remainingSecondsAfterHours = totalSeconds % 3600
-    val minutes: Int = remainingSecondsAfterHours / 60
-    val seconds: Int = remainingSecondsAfterHours % 60
+    val hours: Int = totalSeconds / SECONDS_IN_HOUR
+    val remainingSecondsAfterHours = totalSeconds % SECONDS_IN_HOUR
+    val minutes: Int = remainingSecondsAfterHours / SECONDS_IN_MINUTE
+    val seconds: Int = remainingSecondsAfterHours % SECONDS_IN_MINUTE
 
-    // Форматированный вывод с двумя цифрами
     val formattedTime = String.format("%02d:%02d:%02d", hours, minutes, seconds)
 
     println("Время, проведенное в космосе: $formattedTime")
+
 }
+
+const val SECONDS_IN_HOUR = 3600
+const val SECONDS_IN_MINUTE = 60
