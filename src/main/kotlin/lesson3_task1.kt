9d78abd048debcbb859686c3931b8e2ebe71dd41
khakimov <khakimovmy@gmail.com>
2024-12-17 13:51:38 +0500

    KS-3-1 Welcome message

---
+++ src/main/kotlin/lesson3_task1.kt
@@ -0,0 +1,11 @@
+fun main() {
+
+    val userName = "Joseph"
+    var welcomeMessage = "Добрый день $userName, добро пожаловать в Сбербанк!"
+
+    println(welcomeMessage)
+
+    welcomeMessage = "Добрый вечер $userName, добро пожаловать в Сбербанк!"
+    println(welcomeMessage)
+
+}
