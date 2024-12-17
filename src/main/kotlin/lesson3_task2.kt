2cae4e6dd887c94aeed228908716d9e4d5efbd5d
khakimov <khakimovmy@gmail.com>
2024-12-17 14:42:23 +0500

    KS-3-2 FIO Tatyana

---
+++ src/main/kotlin/lesson3_task2.kt
@@ -0,0 +1,14 @@
+fun main() {
+
+    val name = "Татьяна"
+    val middleName = "Сергеевна"
+    var surname = "Андреева"
+
+    val age20 = "Фамилия: $surname Имя: $name Очество: $middleName"
+    println("В возрасте 20 лет - $age20")
+
+    surname = "Сидорова"
+    val age22 = "Фамилия: $surname Имя: $name Очество: $middleName"
+    println("В возрасте 22 года - $age22")
+
+}
