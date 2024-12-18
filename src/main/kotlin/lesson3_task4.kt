0a94ccc1640f7bc3aabb7800ad02c68b4e48e42f
khakimov <khakimovmy@gmail.com>
2024-12-18 23:07:59 +0500

    KS-3-4 Chess app

--- src/main/kotlin/lesson3_task4.kt
+++ src/main/kotlin/lesson3_task4.kt
@@ -1,17 +1,15 @@
 fun main() {
-
     var fromWhere = "E2"
     var toWhere = "E4"
-    var moveNumber = fromWhere + toWhere
+    var moveNumber = 1
 
     var moveString = "$fromWhere-$toWhere;$moveNumber"
     println("Игрок с белыми сделал ход: $moveString")
 
     fromWhere = "D2"
     toWhere = "D3"
-    moveNumber = fromWhere + toWhere
+    moveNumber++
 
     moveString = "$fromWhere-$toWhere;$moveNumber"
-    println("Игрок с белыми сделал следующий ход: $moveString")
-
+    println("Игрок с черными сделал ход: $moveString")
 }
