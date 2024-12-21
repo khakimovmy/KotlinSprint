b4ebaad7542e9104156e0277efa10ddebe381b6b
khakimov <khakimovmy@gmail.com>
2024-12-21 12:19:13 +0500

    KS-3-5 Chess app final

--- src/main/kotlin/lesson3_task5.kt
+++ src/main/kotlin/lesson3_task5.kt
@@ -2,9 +2,8 @@ fun main() {
 
     val moveString = "D2-D4;0"
 
-    val fromWhere = moveString.substring(0..1)
-    val toWhere = moveString.substring(3..4)
-    val moveNumber = moveString.substring(6)
+    val (fromWhere, toWhere) = moveString.split("-", ";")
+    val moveNumber = moveString.split(";")[1]
 
     println("Игрок сделал ход с $fromWhere на $toWhere. Номер хода: $moveNumber")
 
