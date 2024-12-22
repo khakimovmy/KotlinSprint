dccb5f2692b9b9191040d101aea7d9b7db9cb465
khakimov <khakimovmy@gmail.com>
2024-12-22 22:21:54 +0500

    KS-4-5 Ship can sail

--- src/main/kotlin/lesson4_task5.kt
+++ src/main/kotlin/lesson4_task5.kt
@@ -4,7 +4,6 @@ fun main() {
     val shipCrew = 55
     val provision = 53
     val goodWeather = false
-//    val badWeather = true
 
     val shipCan = (!shipDamages && shipCrew in MIN_CREW..MAX_CREW && provision > MIN_PROVISION) ||
             (shipDamages && shipCrew == RECOMMENDED_CREW && goodWeather && provision >= MIN_PROVISION)
@@ -17,3 +16,6 @@ const val MIN_CREW: Int = 55
 const val MAX_CREW: Int = 70
 const val MIN_PROVISION: Int = 50
 const val RECOMMENDED_CREW: Int = 70
+const val IS_CASE_DAMAGED: Boolean = true
+const val GOOD_WEATHER: Boolean = false
+const val SHIP_DAMAGES = false
