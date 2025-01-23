7f8e214e96483effe7d4f1a2deb4f3d34fa7fc95
khakimov <khakimovmy@gmail.com>
2025-01-24 00:07:44 +0500

    KS-9-3 Ingredients portion

--- src/main/kotlin/lesson_9/lesson9_task3.kt
+++ src/main/kotlin/lesson_9/lesson9_task3.kt
@@ -1,18 +1,17 @@
 package lesson_9
 
 fun main() {
-    val ingredients = mutableListOf(4, 1, 3, 2)
+    val ingredients = listOf(2, 50, 15)
 
     println("Введите количество порций:")
     val portion = readln().toInt()
 
-    val totalEggs = ingredients[0] * portion
-    val totalMilk = ingredients[1] * portion
-    val totalButter = ingredients[2] * portion
-    val totalSalt = ingredients[3] * portion
+    val totalIngredients = ingredients.map { it * portion }
 
-    println(
-        "На $portion порций вам понадобится: Яиц – $totalEggs шт., молока – $totalMilk мл," +
-                "сливочного масла – $totalButter гр., соли – $totalSalt гр."
-    )
+    val ingredientNames = listOf("яиц", "молока", "сливочного масла")
+    val result = ingredientNames.zip(totalIngredients) { name, amount ->
+        "$name – $amount"
+    }.joinToString(", ")
+
+    println("На $portion порций вам понадобится: $result.")
 }
