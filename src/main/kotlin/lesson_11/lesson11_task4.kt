e22a0dbeba5ec263d80553f4bc97190ca1751ff3
khakimov <khakimovmy@gmail.com>
2025-03-16 11:21:28 +0500

    KS-11-4 Burger Ingredient App

--- src/main/kotlin/lesson_11/lesson11_task4.kt
+++ src/main/kotlin/lesson_11/lesson11_task4.kt
@@ -1,7 +1,10 @@
 package lesson_11
 
 class Category(
+    val id: String,
     val name: String,
+    val imageUrl: String,
+    val recipeList: MutableList<Recipe> = mutableListOf(),
 )
 
 class Ingredient(
@@ -12,12 +15,20 @@ class Ingredient(
 class Recipe(
     val title: String,
     val category: Category,
-    val ingredient: List<Ingredient>,
+    val ingredients: List<Ingredient>,
     val instruction: String,
-)
+) {
+    init {
+        category.recipeList.add(this) // Добавляем рецепт в список при создании
+    }
+}
 
 fun main() {
-    val category = Category("Бургеры")
+    val category = Category(
+        "0001",
+        "Бургеры",
+        "https://tse2.mm.bing.net/th?id=OIP.F5Z2UPdYp5m1lVncysw0eQHaFj&pid=Api",
+    )
 
     val burgerIngredient = listOf(
         Ingredient("Творог", "350 г"),
@@ -31,21 +42,31 @@ fun main() {
         Ingredient("Куриное яйцо", "2 штуки"),
     )
 
+println()
+
     val burgerRecipe = Recipe(
         title = "Бургер с грибами и сыром",
         category = category,
-        ingredient = burgerIngredient,
-        instruction = "\n1. В глубокой миске смешайте говяжий фарш, лук, чеснок, соль и перец. Разделите фарш на 4 равные части и сформируйте котлеты.\n"  +
-        "2. Разогрейте сковороду на среднем огне. Обжаривайте котлеты с каждой стороны в течение 4-5 минут или до желаемой степени прожарки.\n" +
-        "3. В то время как котлеты готовятся, подготовьте булочки. Разрежьте их пополам и обжарьте на сковороде до золотистой корочки.\n" +
-        "4. Смазать нижние половинки булочек горчицей и кетчупом, затем положите лист салата, котлету, кольца."
+        ingredients = burgerIngredient,
+        instruction = """
+            1. В глубокой миске смешайте говяжий фарш, лук, чеснок, соль и перец. Разделите фарш на 4 равные части и сформируйте котлеты.
+            2. Разогрейте сковороду на среднем огне. Обжаривайте котлеты с каждой стороны в течение 4-5 минут или до желаемой степени прожарки.
+            3. В то время как котлеты готовятся, подготовьте булочки. Разрежьте их пополам и обжарьте на сковороде до золотистой корочки.
+            4. Смазать нижние половинки булочек горчицей и кетчупом, затем положите лист салата, котлету, кольца.
+        """.trimIndent()
     )
 
     println("Рецепт: ${burgerRecipe.title}")
     println("Категория: ${category.name}")
+    println("Изображение: ${category.imageUrl}")
     println("Ингредиенты:")
-    burgerRecipe.ingredient.forEach {
+    burgerRecipe.ingredients.forEach {
         println(" - ${it.name}: ${it.quantity}")
     }
     println("Инструкция: ${burgerRecipe.instruction}")
+
+    println("\nСписок рецептов в категории '${category.name}':")
+    category.recipeList.forEach {
+        println(" - ${it.title}")
+    }
 }
