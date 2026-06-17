8c0f35ddcf516cc04446eac69ab0cdd24a7f89d7
khakimov <khakimovmy@gmail.com>
2026-06-17 11:14:07 +0500

    KS-3-1 Строки — приветствие в приложении

---
+++ src/main/kotlin/lesson_3/lesson_3_task_1.kt
@@ -0,0 +1,15 @@
+package org.example.lesson_3
+
+fun main() {
+
+    val userName = "Michelle"
+    val day = "Добрый день"
+    val night = "Добрый вечер"
+
+    val dayGreeting = "$day, $userName!"
+    val nightGreeting = "$night, $userName!"
+
+    println(dayGreeting)
+    println(nightGreeting)
+
+}
