380881c0af730ca98b8d9a4c4d08f874156f6e55
khakimov <khakimovmy@gmail.com>
2026-06-17 20:00:41 +0500

    KS-3-4 Методы строки — ход в шахматах

---
+++ src/main/kotlin/lesson_3/lesson_3_task_4.kt
@@ -0,0 +1,16 @@
+package org.example.lesson_3
+
+fun main() {
+
+    var from = "E2"
+    var to = "E4"
+    var moveNumber = 1
+
+    println("$from-$to;$moveNumber")
+
+    from = "D2"
+    to ="D3"
+
+    println("$from-$to;${++moveNumber}")
+
+}
