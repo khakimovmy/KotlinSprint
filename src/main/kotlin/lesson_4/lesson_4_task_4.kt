d86320c0db9f6df6898732827e1e439ab23df0de
khakimov <khakimovmy@gmail.com>
2026-06-18 22:16:08 +0500

    KS-4-4 Инверсия ! — программа тренировок

---
+++ src/main/kotlin/lesson_4/lesson_4_task_4.kt
@@ -0,0 +1,16 @@
+package org.example.lesson_4
+
+fun main() {
+
+    val day = 5
+    val isEven = day % 2 == 0
+    val isNotEven = !isEven
+
+    println("""
+        Упражнения для рук: $isNotEven
+        Упражнения для ног: $isEven
+        Упражнения для спины: $isEven
+        Упражнения для пресса: $isNotEven
+    """.trimIndent())
+
+}
