73ec13d956d33998a618348af653868e008fab2a
khakimov <khakimovmy@gmail.com>
2026-06-21 13:25:20 +0500

    KS-6-5 continue — капча с попытками continue

--- src/main/kotlin/lesson_6/lesson_6_task_5.kt
+++ src/main/kotlin/lesson_6/lesson_6_task_5.kt
@@ -4,36 +4,28 @@ fun main() {
 
     var attempts = 3
 
-    println("Введите логин:")
-    val login = readln()
+    while (attempts > 0) {
+        val num1 = (1..9).random()
+        val num2 = (1..9).random()
 
-    if (login == LOGIN) {
-        println("Введите пароль:")
-        val password = readln()
-        if (password == PASSWORD) {
-            while (attempts > 0) {
-                val num1 = (1..9).random()
-                val num2 = (1..9).random()
+        println("Докажите что вы не бот:\n$num1 + $num2 = ?")
+        val userAnswer = readln().toIntOrNull()
 
-                println("Докажите что вы не бот:\n$num1 + $num2 = ?")
-                val userAnswer = readln().toInt()
+        if (userAnswer == null) {
+            println("Введите число!")
+            continue
+        }
 
-                if (userAnswer == (num1 + num2)) {
-                    println("Добро пожаловать!")
-                    break
+        if (userAnswer == (num1 + num2)) {
+            println("Добро пожаловать!")
+            break
 
-                } else {
-                    println("Неверно!")
-                    attempts--
-                }
-            }
-        } else println("Пароль неверный")
-
-    } else println("Пользователь не найден. Пожалуйста, зарегистрируйтесь.")
+        } else {
+            println("Неверно!")
+            attempts--
+        }
+    }
 
     if (attempts == 0) println("Доступ запрещен")
 
 }
-
-const val LOGIN = "Zaphod"
-const val PASSWORD = "PanGalactic"
