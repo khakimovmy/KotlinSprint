1628e6df15b0b442486829eb7aa729a7d1c514be
khakimov <khakimovmy@gmail.com>
2026-06-21 22:00:03 +0500

    KS-6-4 break — угадай число return

--- src/main/kotlin/lesson_6/lesson_6_task_4.kt
+++ src/main/kotlin/lesson_6/lesson_6_task_4.kt
@@ -2,24 +2,22 @@ package org.example.lesson_6
 
 fun main() {
 
-    val winNumber = 3
+    val winNumber = (1..9).random()
     var attempts = 5
-    var isWin = false
 
     while (attempts > 0) {
         println("Введите число от 1 до 9:")
         val userNumber = readln().toInt()
-        --attempts
 
         if (userNumber == winNumber) {
             println("Это была великолепная игра!")
-            isWin = true
-            break
+            return
         } else {
+            attempts--
             println("Неверно. Осталось ${attempts} попыток")
         }
     }
 
-    if (!isWin) println("Было загадано число $winNumber")
+    println("Было загадано число $winNumber")
 
 }
