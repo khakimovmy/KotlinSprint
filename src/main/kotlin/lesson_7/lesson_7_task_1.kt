9ab473d7f8f8fbbae6ec98a9f5340c2b39f9284a
khakimov <khakimovmy@gmail.com>
2026-06-22 20:57:34 +0500

    KS-7-1 for — генератор пароля

--- src/main/kotlin/lesson_7/lesson_7_task_1.kt
+++ src/main/kotlin/lesson_7/lesson_7_task_1.kt
@@ -5,10 +5,10 @@ import kotlin.random.Random
 fun main() {
 
     val chars = "abcdefghijklmnopqrstuvwxyz"
-    val numbers = "0123456789"
+    val digits = "0123456789"
     var password = ""
     for (i in 1..6) {
-        if (i % 2 == 0) password += numbers[Random.nextInt(numbers.length)]
+        if (i % 2 == 0) password += digits[Random.nextInt(digits.length)]
         else password += chars[Random.nextInt(chars.length)]
     }
     println(password)
