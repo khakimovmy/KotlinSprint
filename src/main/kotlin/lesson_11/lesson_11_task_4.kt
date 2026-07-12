2c785265534956a4be6a06ed32905492af734d73
khakimov <khakimovmy@gmail.com>
2026-07-12 13:38:46 +0500

    KS-11-4 Методы — RecipesApp

--- src/main/kotlin/lesson_11/lesson_11_task_4.kt
+++ src/main/kotlin/lesson_11/lesson_11_task_4.kt
@@ -1,23 +1,25 @@
 package org.example.lesson_11
 
 class Category(
-    val photo: String,
+    val photoUrl: String,
     val name: String,
     val description: String,
+    val recipe: List<Recipe>
 )
 
 class Recipe(
+    val photoUrl: String,
     val name: String,
     val category: Category,
     val ingredients: List<Ingredient>,
     val time: Int,
     val portions: Int,
     val difficulty: String = "Easy",
-    val instruction: String,
+    val instruction: List<String>,
 )
 
 class Ingredient(
     val ingredientName: String,
-    val ingredientAmount: Int,
+    val ingredientAmount: Float,
+    val units: String,
 )
-
