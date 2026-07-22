00c0cea1c41aaa0b5af832545ce7c20fb09d0f36
khakimov <khakimovmy@gmail.com>
2026-07-23 00:50:42 +0500

    KS-12-3 Secondary constructor — погода в Кельвинах

--- src/main/kotlin/lesson_12/lesson_12_task_3.kt
+++ src/main/kotlin/lesson_12/lesson_12_task_3.kt
@@ -5,8 +5,8 @@ class Weather3(
     nightTemp: Int,
     isRain: Boolean,
 ) {
-    val dayTemperature: Int = dayTemp - 273
-    val nightTemperature: Int = nightTemp - 273
+    val dayTemperature: Int = dayTemp - TO_CELSIUS
+    val nightTemperature: Int = nightTemp - TO_CELSIUS
     val precipitation: Boolean = isRain
 
     fun report() {
@@ -21,3 +21,5 @@ fun main() {
     day1.report()
     day2.report()
 }
+
+const val TO_CELSIUS = 273
