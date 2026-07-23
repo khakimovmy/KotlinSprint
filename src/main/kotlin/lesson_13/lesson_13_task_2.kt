8558e599bb83408ed5b5fa32535210f296909506
khakimov <khakimovmy@gmail.com>
2026-07-23 23:03:25 +0500

    KS-13-2 Оператор ?. — безопасный вызов

---
+++ src/main/kotlin/lesson_13/lesson_13_task_2.kt
@@ -0,0 +1,20 @@
+package org.example.lesson_13
+
+class Phone2(
+    val name: String,
+    val phone: Long,
+    val company: String? = null,
+) {
+    fun report() {
+        println(
+            "Имя: $name\n" +
+                    "Номер: $phone\n" +
+                    "Компания: ${company?: "не указано"}"
+        )
+    }
+}
+
+fun main() {
+    val phone1 = Phone2("Anna", 1234567890,)
+    phone1.report()
+}
