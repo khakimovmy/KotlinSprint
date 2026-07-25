aa73867c4175d46423edac5d47271f072b62f4b6
khakimov <khakimovmy@gmail.com>
2026-07-26 01:24:26 +0500

    KS-14-2 override — корабли с погрузкой

---
+++ src/main/kotlin/lesson_14/lesson_14_task_2.kt
@@ -0,0 +1,56 @@
+package org.example.lesson_14
+
+open class Liner2(
+    open val speed: Int = 70,
+    open val payload: Int = 0,
+    open val capacity: Int = 100
+) {
+    open fun getInfo() {
+        println("$speed, $payload, $capacity")
+    }
+
+    open fun loadingMethod() {
+        println("лайнер выдвигает горизонтальный трап со шкафута")
+    }
+}
+
+class Cargo2 : Liner2() {
+    override val speed = 30
+    override val payload = 100
+    override val capacity = 10
+
+    override fun getInfo() {
+        println("$speed, $payload, $capacity")
+    }
+
+    override fun loadingMethod() {
+        println("грузовой корабль активирует погрузочный кран")
+    }
+}
+
+class Icebreaker2 : Liner2() {
+    override val speed = 30
+    override val capacity = 10
+    val canBreakIce = true
+
+    override fun getInfo() {
+        println("$speed, $payload, $capacity $canBreakIce")
+    }
+
+    override fun loadingMethod() {
+        println("ледокол открывает ворота со стороны кормы.")
+    }
+}
+
+fun main() {
+    val liner = Liner2()
+    val cargo = Cargo2()
+    val icebreaker = Icebreaker2()
+
+    liner.getInfo()
+    liner.loadingMethod()
+    cargo.getInfo()
+    cargo.loadingMethod()
+    icebreaker.getInfo()
+    icebreaker.loadingMethod()
+}
