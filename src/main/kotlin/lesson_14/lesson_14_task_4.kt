0402cd7857f68c2ad73c12d932ddb0ba7e4f9e79
khakimov <khakimovmy@gmail.com>
2026-07-26 04:56:15 +0500

    KS-14-4 Цепочка наследования — планеты

---
+++ src/main/kotlin/lesson_14/lesson_14_task_4.kt
@@ -0,0 +1,29 @@
+package org.example.lesson_14
+
+open class CelestialBody(
+    val name: String,
+    val atmosphere: Boolean,
+    val canLand: Boolean
+)
+
+class Planet(
+    name: String,
+    atmosphere: Boolean,
+    canLand: Boolean,
+    val satellites: List<Satellite>
+) : CelestialBody(name, atmosphere, canLand)
+
+class Satellite(
+    name: String,
+    atmosphere: Boolean,
+    canLand: Boolean
+) : CelestialBody(name, atmosphere, canLand)
+
+fun main() {
+    val moon = Satellite("Луна", false, true)
+    val europa = Satellite("Европа", true, false)
+    val earth = Planet("Земля", true, true, listOf(moon, europa))
+
+    println(earth.name)
+    earth.satellites.forEach { println(it.name) }
+}
