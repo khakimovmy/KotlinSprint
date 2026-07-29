9ea7c35b6f63759fe24b56f728d04c2b40c8f909
khakimov <khakimovmy@gmail.com>
2026-07-30 03:44:45 +0500

    KS-17-3 getter с логикой — облачные файлы

---
+++ src/main/kotlin/lesson_17/lesson_17_task_3.kt
@@ -0,0 +1,18 @@
+package org.example.lesson_17
+
+class Folder(val name: String, val filesAmount: Int, var isSecret: Boolean) {
+
+    val folderName: String
+        get() = if (isSecret) "скрытая папка"
+        else name
+
+    val filesCount: Int
+        get() = if (isSecret) 0
+        else filesAmount
+}
+
+fun main() {
+    val folder1 = Folder("Personal", 20, true)
+    println(folder1.folderName)
+    println(folder1.filesCount)
+}
