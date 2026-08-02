6e4b350a75db16258dcf37c9815ff763f1b1c68b
khakimov <khakimovmy@gmail.com>
2026-08-02 23:55:47 +0500

    KS-18-5 toString() — фигуры на экране

--- src/main/kotlin/lesson_18/lesson_18_task_5.kt
+++ src/main/kotlin/lesson_18/lesson_18_task_5.kt
@@ -1,37 +1,25 @@
 package org.example.lesson_18
 
 class Screen {
+    fun drawPoint(x: Int, y: Int): String = "Точка: x=$x, y=$y"
+    fun drawPoint(x: Float, y: Float): String = "Точка: x=$x, y=$y"
 
-    fun draw(pointX: Int, pointY: Int): String =
-        "Точка: x=$pointX, y=$pointY"
+    fun drawCircle(x: Int, y: Int): String = "Круг: x=$x, y=$y"
+    fun drawCircle(x: Float, y: Float): String = "Круг: x=$x, y=$y"
 
-    fun draw(pointX: Float, pointY: Float): String =
-        "Точка: x=$pointX, y=$pointY"
-
-
-    fun draw(circleX: Int, circleY: Int, radius: Int): String =
-        "Круг: x=$circleX, y=$circleY"
-
-    fun draw(circleX: Float, circleY: Float, radius: Float): String =
-        "Круг: x=$circleX, y=$circleY"
-
-
-    fun draw(squareX: Int, squareY: Int, side: String): String =
-        "Квадрат: x=$squareX, y=$squareY"
-
-    fun draw(squareX: Float, squareY: Float, side: String): String =
-        "Квадрат: x=$squareX, y=$squareY"
+    fun drawSquare(x: Int, y: Int): String = "Квадрат: x=$x, y=$y"
+    fun drawSquare(x: Float, y: Float): String = "Квадрат: x=$x, y=$y"
 }
 
 fun main() {
     val screen = Screen()
 
-    println(screen.draw(1, 2))
-    println(screen.draw(1.5f, 2.5f))
+    println(screen.drawPoint(1, 2))
+    println(screen.drawPoint(1.5f, 2.5f))
 
-    println(screen.draw(3, 4, 10))
-    println(screen.draw(3.5f, 4.5f, 10f))
+    println(screen.drawCircle(3, 4))
+    println(screen.drawCircle(3.5f, 4.3f))
 
-    println(screen.draw(5, 6, ""))
-    println(screen.draw(5.5f, 6.5f, ""))
+    println(screen.drawSquare(5, 6))
+    println(screen.drawSquare(5.5f, 6.5f))
 }
