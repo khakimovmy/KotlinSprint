607fba147f0ee8f7c27bd5bdfb0af94aee01815d
khakimov <khakimovmy@gmail.com>
2026-08-03 22:20:20 +0500

    KS-19-3 when + enum — космический корабль

---
+++ src/main/kotlin/lesson_19/lesson_19_task_3.kt
@@ -0,0 +1,15 @@
+package org.example.lesson_19
+
+class SpaceShip {
+    fun fly() {
+        // TODO: Need logic
+    }
+
+    fun land() {
+        println("Landing")
+    }
+
+    fun shoot() {
+        TODO("Shooting")
+    }
+}
