18f1275a495c0e7b933d741c0f4f5079505f2cdb
khakimov <khakimovmy@gmail.com>
2026-08-05 04:19:04 +0500

    KS-20-2 лямбда с параметром — лечебное зелье

--- src/main/kotlin/lesson_20/lesson_20_task_2.kt
+++ src/main/kotlin/lesson_20/lesson_20_task_2.kt
@@ -1,11 +1,11 @@
 package org.example.lesson_20
 
-class Player(val name: String, var health: Int, val maxHealth: Int)
+class Player1(val name: String, var health: Int, val maxHealth: Int)
 
-val heal: (Player) -> Unit = { player -> player.health = player.maxHealth}
+val heal: (Player1) -> Unit = { player -> player.health = player.maxHealth}
 
 fun main() {
-    val player1 = Player("Alex", 50, 100)
+    val player1 = Player1("Alex", 50, 100)
     println(player1.health)
     heal(player1)
     println(player1.health)
