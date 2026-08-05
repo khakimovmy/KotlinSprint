49c3e274ab336f49d9f20a44ef4ded66b86f2945
khakimov <khakimovmy@gmail.com>
2026-08-06 00:42:16 +0500

    KS-21-3 extension на класс — isHealthy для игрока

--- src/main/kotlin/lesson_21/lesson_21_task_3.kt
+++ src/main/kotlin/lesson_21/lesson_21_task_3.kt
@@ -1,10 +1,12 @@
 package org.example.lesson_21
 
-data class Player21(val name: String, val hp: Int, val maxHp: Int)
+data class Player(val name: String, val hp: Int, val maxHp: Int)
 
-fun Player21.isHealthy(): Boolean = hp == maxHp
+fun Player.isHealthy(): Boolean = hp == maxHp
 
 fun main() {
-    val player1 = Player21("Kamila", 100, 100)
+    val player1 = Player("Kamila", 100, 100)
+    val player2 = Player("Sadullayeva", 99, 100)
     println(player1.isHealthy())
+    println(player2.isHealthy())
 }
