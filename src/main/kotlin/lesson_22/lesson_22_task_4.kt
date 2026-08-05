9e6076d65f7901bc3f341fffc6a7e51c2908726f
khakimov <khakimovmy@gmail.com>
2026-08-06 03:42:47 +0500

    KS-22-4 equals и hashCode — MVVM состояние

--- src/main/kotlin/lesson_22/lesson_22_task_4.kt
+++ src/main/kotlin/lesson_22/lesson_22_task_4.kt
@@ -1,13 +1,15 @@
 package org.example.lesson_22
 
 class MainScreenViewModel {
-    var mainScreenState = MainScreenState("")
+    data class MainScreenState(
+        val data: String? = null,
+        val isLoading: Boolean = false,
+    )
+
+    var mainScreenState = MainScreenState()
 
     fun loadData() {
         mainScreenState = mainScreenState.copy(isLoading = true)
         mainScreenState = mainScreenState.copy(data = "Loaded Data", isLoading = false)
     }
-}
-
-data class MainScreenState(val data: String, val isLoading: Boolean = false) {
 }
