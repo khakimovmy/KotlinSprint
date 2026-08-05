f9105bc926ef3547f785df0af5fea162cfe48d09
khakimov <khakimovmy@gmail.com>
2026-08-06 01:04:07 +0500

    KS-21-4 extension на File — телеграм-бот словарь

---
+++ src/main/kotlin/lesson_21/lesson_21_task_4.kt
@@ -0,0 +1,20 @@
+package org.example.lesson_21
+
+import java.io.File
+
+fun File.writeWord(word: String) {
+    val lowerWord = word.lowercase()
+
+    val oldText = if (exists()) readText()
+    else ""
+
+    val newText = if (oldText.isEmpty()) lowerWord
+    else lowerWord + "\n" + oldText
+
+    writeText(newText)
+}
+
+fun main() {
+    val file = File("words.txt")
+    file.writeWord("Hello")
+}
